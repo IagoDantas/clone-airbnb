db4dd81ea5d9b6abc8607b17a65e0dac0c98ade5
Iago Dantas <iagodantas.8@gmail.com>
2023-06-26 15:51:37 -0300

    some parts of auth ui

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+interface ClientOnlyProps {
+  children: React.ReactNode
+}
+
+export default function ClientOnly({ children }: ClientOnlyProps) {
+  const [hasMounted, setHasMounted] = useState(false)
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  if (!hasMounted) {
+    return null
+  }
+  return (
+    <>
+      {children}
+    </>
+  )
+}
