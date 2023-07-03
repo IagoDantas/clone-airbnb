2d94b92daf1f3b18eac7cd37e320f9f4eb67ca16
Iago Dantas <iagodantas.8@gmail.com>
2023-07-03 16:32:54 -0300

    categories ui

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Container } from "../Container";
+import { Categories } from "./Categories";
 import { Logo } from "./Logo";
 import { Search } from "./Search";
 import { UserMenu } from "./UserMenu";
@@ -30,6 +31,7 @@ export default function Navbar({ currentUser }: NavbarProps) {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   )
 }
