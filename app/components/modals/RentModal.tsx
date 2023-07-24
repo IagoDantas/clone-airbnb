5916d2ffa197a034db7088712476665c276878fd
Iago Dantas <iagodantas.8@gmail.com>
2023-07-24 11:27:56 -0300

    add image upload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-
 import { useRentModal } from "@/app/hooks/useRentModal";
 import Modal from "./Modal";
 import { useMemo, useState } from "react";
@@ -11,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import { CountrySelect } from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import { Counter } from "../inputs/Counter";
+import { ImageUpload } from "../inputs/ImageUpload";
 
 
 enum STEPS {
@@ -55,6 +55,7 @@ export function RentModal() {
   const guestsCount = watch('guestCount')
   const roomCount = watch('roomCount')
   const bathroomCount = watch('bathroomCount')
+  const imageSrc = watch('imageSrc')
 
   const Map = useMemo(() => dynamic(() => import('../Map').then((module) => module.Map), {
     ssr: false
@@ -158,6 +159,21 @@ export function RentModal() {
           subtitle="How many bathrooms do you have?"
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
         />
       </div>
     )
