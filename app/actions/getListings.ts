a83d9185585c0f68f755e4fa6afde1829444a3e0
Iago Dantas <iagodantas.8@gmail.com>
2023-07-31 11:28:53 -0300

    safeListing type

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,6 +1,5 @@
 import prisma from '@/app/libs/prismadb'
 
-
 export default async function getListings(){
   try{
     const listings = await prisma.listing.findMany({
@@ -8,7 +7,12 @@ export default async function getListings(){
         createdAt: 'desc'
       }
     });
-    return listings
+    const safeListings =   listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }))
+
+    return safeListings;
   }
   catch(error: any){
     throw new Error(error)
