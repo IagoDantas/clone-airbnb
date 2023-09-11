9bbb4998bce871529043a404cfaa2a818419efae
Iago Dantas <iagodantas.8@gmail.com>
2023-09-11 12:28:24 -0300

    favorite's screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -60,7 +60,7 @@ export function UserMenu({ currentUser }: UserMenuProps) {
               (
                 <>
                   <MenuItem onClick={() => router.push("/trips")} label='My trips' />
-                  <MenuItem onClick={() => { }} label='My favorites' />
+                  <MenuItem onClick={() => router.push("/favorites")} label='My favorites' />
                   <MenuItem onClick={() => router.push("/reservations")} label='My reservations' />
                   <MenuItem onClick={() => { }} label='My properties' />
                   <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
